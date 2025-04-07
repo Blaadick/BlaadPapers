7238fc65fc5903dc0fed71edc4d31c47a8a3f75b
Blaadick <lablaadick@gmail.com>
2025-04-07 22:01:57 +0300

    Connect QT

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <filesystem>
 #include <fstream>
+#include <QApplication>
+#include <QGridLayout>
+#include <QPushButton>
 #include <json/json.hpp>
 
 #include "Defaults.hpp"
@@ -59,13 +62,49 @@ void loadWallpapers() {
     }
 }
 
-int main(const int argc, const char **argv) {
+int main(int argc, const char **argv) {
     readConfig();
     loadWallpapers();
 
     if(argc >= 2) {
         OptionExecutor::getInstance().execute(argv);
     } else {
-        //TODO Run GUI application
+        QApplication app(argc, nullptr);
+        QWidget window;
+
+        QGridLayout grid(&window);
+        grid.setSpacing(10);
+
+        QPushButton btn00("00", &window);
+        QPushButton btn01("01", &window);
+        QPushButton btn02("02", &window);
+        QPushButton btn10("10", &window);
+        QPushButton btn11("11", &window);
+        QPushButton btn12("12", &window);
+        QPushButton btn20("20", &window);
+        QPushButton btn21("21", &window);
+        QPushButton btn22("22", &window);
+
+        const auto buttons = {&btn00, &btn01, &btn02, &btn10, &btn11, &btn12, &btn20, &btn21, &btn22};
+
+        for(QPushButton *button: buttons) {
+            button->setFixedSize(50, 50);
+        }
+
+        grid.addWidget(&btn00, 0, 0);
+        grid.addWidget(&btn01, 0, 1);
+        grid.addWidget(&btn02, 0, 2);
+
+        grid.addWidget(&btn10, 1, 0);
+        grid.addWidget(&btn11, 1, 1);
+        grid.addWidget(&btn12, 1, 2);
+
+        grid.addWidget(&btn20, 2, 0);
+        grid.addWidget(&btn21, 2, 1);
+        grid.addWidget(&btn22, 2, 2);
+
+        window.show();
+
+        return QApplication::exec();
     }
 }
