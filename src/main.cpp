d827acff24aa53e6b8b6c426fa39c75581d61729
Blaadick <lablaadick@gmail.com>
2025-04-13 00:14:42 +0300

    Build GUI layout

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include <filesystem>
 #include <fstream>
 #include <QApplication>
-#include <QApplication>
-#include <QComboBox>
-#include <QDebug>
-#include <QLineEdit>
-#include <QListWidget>
-#include <QMainWindow>
-#include <QScreen>
-#include <QSplitter>
-#include <QVBoxLayout>
-#include <QPushButton>
 #include <json/json.hpp>
 
 #include "Defaults.hpp"
 #include "Global.hpp"
 #include "OptionExecutor.hpp"
+#include "qt/MainWindow.hpp"
 
 using namespace std;
 using namespace filesystem;
@@ -70,70 +61,15 @@ void loadWallpapers() {
     }
 }
 
-QSize getAspectRatio(const QRect &geometry) {
-    const int divisor = std::gcd(geometry.width(), geometry.height());
-    return {geometry.width() / divisor, geometry.height() / divisor};
-}
-
-int main(int argc, const char **argv) {
+int main(int argc, char **argv) {
     readConfig();
     loadWallpapers();
 
     if(argc >= 2) {
         OptionExecutor::getInstance().execute(argv);
     } else {
-        QApplication app(argc, nullptr);
-        QMainWindow mainWindow;
-
-        for(const auto screen: QGuiApplication::screens()) {
-            qDebug() << screen->name() << ":";
-            qDebug() << "  Physical Size:" << screen->physicalSize().width() << "x" << screen->physicalSize().height() << "mm";
-            qDebug() << "  Pixel Size:" << screen->geometry().width() << "x" << screen->geometry().height();
-        }
-
-        const auto splitter = new QSplitter();
-        const auto wallpaperDetails = new QListWidget();
-        wallpaperDetails->setFixedWidth(300);
-
-        const auto topBatWidget = new QWidget();
-        const auto topBarLayout = new QHBoxLayout(topBatWidget);
-        const auto searchBox = new QLineEdit();
-        const auto monitorCombo = new QComboBox();
-        topBarLayout->addWidget(monitorCombo);
-        topBarLayout->addWidget(searchBox);
-
-        const auto rightWidget = new QWidget();
-        const auto mainLayout = new QVBoxLayout(rightWidget);
-        const auto wallpaperList = new QGridLayout();
-
-        wallpaperList->setSpacing(10);
-        wallpaperList->setAlignment(Qt::AlignCenter);
-
-        mainLayout->addWidget(topBatWidget, 0, Qt::AlignTop);
-        mainLayout->addLayout(wallpaperList, 0);
-
-        splitter->addWidget(wallpaperDetails);
-        splitter->addWidget(rightWidget);
-
-        splitter->setChildrenCollapsible(false);
-
-        mainWindow.setCentralWidget(splitter);
-
-        int i = 0;
-        for(const auto &wallpaper: wallpapers) {
-            if(i >= 10) break;
-
-            const auto wallpaperWidget = new QPushButton(QIcon(wallpaper.getFilePath().c_str()), wallpaper.getName().c_str());
-
-            wallpaperWidget->setFixedSize(getAspectRatio(QGuiApplication::screens()[0]->geometry()) * 10);
-
-            wallpaperList->addWidget(wallpaperWidget, i / 4, i % 4);
-
-            i++;
-        }
-
-        mainWindow.show();
-
+        QApplication app(argc, argv);
+        MainWindow::getInstance().show();
         return QApplication::exec();
     }
 }
